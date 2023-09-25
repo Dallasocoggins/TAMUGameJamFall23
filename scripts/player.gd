80e67c4a1e2a29eca97f7a6b568f2de40d5864f7
PeterSchlenker <91510378+PeterSchlenker@users.noreply.github.com>
2023-09-25 00:14:50 -0500

    Modified player size and movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,18 +1,23 @@
 extends CharacterBody2D
 
 
-@export var speed = 300.0
+@export var run_max_speed = 500.0
+@export var run_acceleration = 2000.0
+@export var run_deceleration = 4000.0
+@export var turn_acceleration = 10000.0
 
+@export var jump_velocity = -400.0
+@export var jump_low_multiplier = 2 # Used for falling faster when jump button is let go of early
+@export var fall_acceleration_multiplier = 1.5
+@export var terminal_velocity = 1000.0
+@export var jump_count_max = 2
 
-@export var jumpVelocity = -400.0
-@export var jumpCountMax = 2
-@export var jumpLowMultiplier = 2; # Used for falling faster when jump button is let go of early
-var jumpCountCurrent = jumpCountMax
+@export var dash_speed = 900
 
-@export var fallMultipler = 2.5
-
-var dashEnabled = true
-@export var dashSpeed = 900
+var last_velocity_sign = 1
+var jump_count_current = jump_count_max
+var dash_enabled = true
+@onready var sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -25,24 +30,33 @@ func _physics_process(delta):
 	
 	# Add extra forces for faster falling after jumping and to end jump early
 	if velocity.y >= 0 && not is_on_floor():
-		velocity.y += gravity * (fallMultipler - 1) * delta
-	elif velocity.y < 0 && not is_on_floor() && not Input.is_action_pressed("ui_accept"):
-		velocity.y += gravity * (jumpLowMultiplier - 1) * delta
+		velocity.y += gravity * (fall_acceleration_multiplier - 1) * delta
+	elif velocity.y < 0 && not is_on_floor() && not Input.is_action_pressed("jump"):
+		velocity.y += gravity * (jump_low_multiplier - 1) * delta
 	elif is_on_floor():
-		jumpCountCurrent = jumpCountMax
+		jump_count_current = jump_count_max
+		
+	# You can't fall faster than terminal velocity
+	velocity.y = min (velocity.y, terminal_velocity)
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and jumpCountCurrent >= 1:
-		velocity.y = jumpVelocity
-		jumpCountCurrent -= 1
+	if Input.is_action_just_pressed("jump") and jump_count_current >= 1:
+		velocity.y = jump_velocity
+		jump_count_current -= 1
 	
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good pactice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * speed
+		velocity.x += direction * delta * (run_acceleration if sign(direction) == sign(velocity.x) else turn_acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		var new_velocity = velocity.x - sign(velocity.x) * delta * run_deceleration
+		velocity.x = new_velocity if sign(new_velocity) == sign(velocity.x) else 0
+		
+	velocity.x = clampf(velocity.x, -run_max_speed, run_max_speed)
+	
+	if (sign(velocity.x) != last_velocity_sign && sign(velocity.x) != 0):
+		sprite.flip_h = sign(velocity.x) <  0
+		last_velocity_sign = sign(velocity.x)
 
 	move_and_slide()
