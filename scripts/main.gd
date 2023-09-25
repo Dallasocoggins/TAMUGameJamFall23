fb1fff94adf8bfb101fd7f3ad827b95d3101f904
PeterSchlenker <91510378+PeterSchlenker@users.noreply.github.com>
2023-09-25 00:55:10 -0500

    Tweaked the camera, added a main scene, and fixed character jittering

---
+++ scripts/main.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# I was having an issue where the character jitters when the camera moves.
+	# It turns out setting the physics framerate to match the monitor refresh rate fixes the issue.
+	Engine.physics_ticks_per_second = DisplayServer.screen_get_refresh_rate()
