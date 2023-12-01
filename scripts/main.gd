e651907293e2917d9e679a7f8d4c96634144182c
PeterSchlenker <91510378+PeterSchlenker@users.noreply.github.com>
2023-11-30 22:37:24 -0600

    Added pause menu

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
+var in_level = false
+var pause_screen = null
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
 	load_main_menu()
 	
 	# I was having an issue where the character jitters when the camera moves.
@@ -9,14 +14,20 @@ func _ready():
 	Engine.physics_ticks_per_second = DisplayServer.screen_get_refresh_rate()
 
 func _process(delta):
-	if Input.is_action_just_pressed("pause"):
-		get_tree().quit()
+	if Input.is_action_just_pressed("pause") && in_level:
+		# get_tree().quit()
+		if pause_screen == null:
+			pause()
+		else:
+			unpause()
 
 func unload_children():
 	for node in get_children():
 		remove_child(node)
 
 func load_main_menu():
+	in_level = false
+	unpause()
 	unload_children()
 	var scene = preload("res://scenes/menus/main_menu.tscn")
 	var instance = scene.instantiate()
@@ -35,7 +46,20 @@ func load_credits():
 	add_child(instance)
 
 func load_level():
+	in_level = true
 	unload_children()
 	var scene = preload("res://scenes/level/level2.tscn")
 	var instance = scene.instantiate()
+	instance.process_mode = Node.PROCESS_MODE_PAUSABLE
 	add_child(instance)
+
+func pause():
+	get_tree().paused = true
+	var scene = preload("res://scenes/menus/pause_menu.tscn")
+	pause_screen = scene.instantiate()
+	add_child(pause_screen)
+
+func unpause():
+	get_tree().paused = false
+	remove_child(pause_screen)
+	pause_screen = null
---
+++ scripts/ui_ux/pause_menu/Credits.gd
@@ -0,0 +1,7 @@
+extends TextureButton
+
+
+
+func _on_pressed():
+	var main = get_node("/root/Main")
+	main.load_credits()
